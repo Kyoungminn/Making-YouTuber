110d9d3d72a8590b6459865d613e2cb4a1178c99
suabae <bsa0322@ewhain.net>
2021-01-08 22:51:08 +0900

    UploadScene

---
+++ youtuber_minn/Assets/Scripts/CameraResolution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResolution : MonoBehaviour
+{
+
+    private void Start()
+    {
+       // Screen.SetResolution(Screen.width, Screen.width * 9 / 16, true);
+       float fScaleHeight = ((float)Screen.height / (float)Screen.width) / ((float)16 / (float)9);
+
+        Vector3 vecButtonPos = GetComponent<RectTransform>().localPosition;
+        vecButtonPos.y = vecButtonPos.y * fScaleHeight;
+        GetComponent<RectTransform>().localPosition = new Vector3(vecButtonPos.x, vecButtonPos.y, vecButtonPos.z);
+    }
+}
