ae3c95f1b8fef18d8521116a85968dbc1ab41619
minkyoung <minnkyoung2014@gmail.com>
2021-01-20 23:11:03 +0900

    멤버십UI추가/화면이동추가

---
+++ youtuber_minn/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void StoreChange()
+    {
+        SceneManager.LoadScene("Store_scene");
+    }
+    public void LockerChange()
+    {
+        SceneManager.LoadScene("Locker_scene");
+    }
+     public void MembershipChange()
+    {
+        SceneManager.LoadScene("Membership_scene");
+    }
+    public void UploadChange()
+    {
+        SceneManager.LoadScene("Upload_scene");
+    }
+     public void MainChange()
+    {
+        SceneManager.LoadScene("Main_scene");
+    }
+
+
+
+}
