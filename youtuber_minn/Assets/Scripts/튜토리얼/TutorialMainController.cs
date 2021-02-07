8e3baa005ce90566240a70affcb276afd386bac0
suabae <bsa0322@ewhain.net>
2021-02-07 15:36:39 +0900

    튜토리얼끝!

--- youtuber_minn/Assets/Scripts/튜토리얼/TutorialMainController.cs
+++ youtuber_minn/Assets/Scripts/튜토리얼/TutorialMainController.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class TutorialMainController : MonoBehaviour
 {
+    public GameObject click_pannel, click_pannel2;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         gameObject.transform.GetChild(1).gameObject.GetComponent<Text>().text = GameManager.human_name;
         gameObject.transform.GetChild(2).gameObject.GetComponent<Text>().text = "( "+GameManager.channel_name+" )";
@@ -18,11 +19,24 @@ public class TutorialMainController : MonoBehaviour
         GameManager.health = 51;
         GameManager.subscriber = 100;
         GameManager.money = 1000;
+        GameObject.Find("Panel_Profile").transform.GetChild(6).gameObject.GetComponent<Text>().text = GameManager.money.ToString();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        gameObject.transform.GetChild(6).gameObject.GetComponent<Text>().text = GameManager.money.ToString();
+    { 
+        
+        if(click_pannel)
+        {
+            Text _name = click_pannel.transform.GetChild(4).GetComponent<Text>();
+            _name.text = GameManager.human_name;
+        }
+
+        if(click_pannel2)
+        {
+            Text _name = click_pannel2.transform.GetChild(4).GetComponent<Text>();
+            _name.text = GameManager.human_name;
+        }
+    
     }
 }
