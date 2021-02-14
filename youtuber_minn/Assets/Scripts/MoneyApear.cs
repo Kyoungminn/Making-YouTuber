565e6d5c3d0a4edc85e00c67749db93b53738472
suabae <bsa0322@ewhain.net>
2021-02-15 03:23:17 +0900

    미니게임구현+캐릭터눈깜빡임

---
+++ youtuber_minn/Assets/Scripts/MoneyApear.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyApear : MonoBehaviour
+{
+    public GameObject prefabMoney; //돈 프리팹
+    public GameObject moneyParent; //캔버스
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ApearMoney());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator ApearMoney()
+    {
+        while(true)
+        {
+            int numRand = Random.Range(1, 3);
+            float timeRand = Random.Range(6.0f, 10.0f);
+
+            for (int i = 0; i < numRand; i++)
+            {
+                Debug.Log("생성!");
+                Vector3 creatingPoint = new Vector3(Random.Range(-500f, 500f), Random.Range(-900f, 900f), 0f);
+                GameObject child = Instantiate(prefabMoney) as GameObject;
+                child.transform.SetParent(moneyParent.transform);
+                child.transform.localPosition = creatingPoint;
+                child.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            }
+
+            yield return new WaitForSeconds(timeRand);
+        }
+    }
+}
---
+++ youtuber_minn/Assets/Scripts/SpriteRendererToUguiImage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SpriteRendererToUguiImage : MonoBehaviour
+{
+
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
+
+    [SerializeField]
+    private Image _image;
+
+    public Image DarkCurcle;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (_spriteRenderer == null)
+            _spriteRenderer = this.GetComponent<SpriteRenderer>();
+
+        _spriteRenderer.enabled = false;
+
+        if (_image == null)
+            _image = this.GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _image.sprite = _spriteRenderer.sprite;
+
+        if (GameManager.health < 10) DarkCurcle.gameObject.SetActive(true);
+        else DarkCurcle.gameObject.SetActive(false);
+
+    }
+}
